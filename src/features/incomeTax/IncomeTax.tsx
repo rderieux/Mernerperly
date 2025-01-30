135218b1778978834c0521ef2442944bfe73b955
Robert DeRieux <ppldiecomplex@gmail.com>
2025-01-30 15:17:58 -0700

    add income tax component

---
+++ src/features/incomeTax/IncomeTax.tsx
@@ -0,0 +1,22 @@
+import "./IncomeTax.css";
+
+export default function IncomeTax() {
+  return (
+    <div className="income-tax">
+      <div className="income-tax-info">
+        <p className="income-tax-name">
+          INCOME
+          <br /> TAX
+          <br />♦
+        </p>
+        <p className="income-tax-description">
+          PAY 10%
+          <br />
+          OR
+          <br />
+          $200
+        </p>
+      </div>
+    </div>
+  );
+}
