439b8f14c2b6eb26c06b78315384ffd79db3276f
Robert DeRieux <ppldiecomplex@gmail.com>
2025-02-06 17:02:39 -0700

    Add the red arrow, fix the board color, fix the chance question mark color

--- src/features/start/Start.tsx
+++ src/features/start/Start.tsx
@@ -1,12 +1,20 @@
+import arrow from "../../assets/arrow.png";
 import "./Start.css";
 
 export default function Start() {
   return (
-    <div className="start">
-      <p>COLLECT</p>
-      <p>$200.00 SALARY</p>
-      <p>AS YOU PASS</p>
-      <p className="go">GO</p>
-    </div>
+    <>
+      <div className="start-container">
+        <div className="start">
+          <p>COLLECT</p>
+          <p>$200.00 SALARY</p>
+          <p>AS YOU PASS</p>
+          <p className="go">GO</p>
+        </div>
+        <div className="arrow-container">
+          <img src={arrow} className="arrow" alt="Red Arrow" />
+        </div>
+      </div>
+    </>
   );
 }
